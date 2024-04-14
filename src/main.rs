7ebd82856f0d68a26c457b3416ac5995fe0a22c1
Akshay S <akshay.s@Akshay-Subramanian-D66TQ6D97K.local>
2024-04-14 18:10:51 +0530

    2nd stage port

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     
-    let listener = TcpListener::bind("127.0.0.1:6370").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
