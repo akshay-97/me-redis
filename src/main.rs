073aaab88b22f0ebca1bcb19b537848fe91cbaa9
Akshay S <akshay.s@Akshay-Subramanian-D66TQ6D97K.local>
2024-04-16 15:02:47 +0530

    try

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn handle_client(mut s : TcpStream) -> &'static str{
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    let listener = TcpListener::bind("127.0.0.1:6370").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
