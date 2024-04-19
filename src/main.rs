45f6e79a8650e17585c4077ea5376aeab8c4ff7f
Akshay S <akshay.s@Akshay-Subramanian-D66TQ6D97K.local>
2024-04-19 10:57:55 +0530

    shit pool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 // Uncomment this block to pass the first stage
 use std::{io::Write, io::Read, net::{TcpListener, TcpStream}};
 
-fn handle_client(mut s : TcpStream) -> &'static str{
+fn handle_client(mut s : TcpStream){
     let mut buf = [0;512];
     loop {
         
         let count = s.read(&mut buf).expect("read stream");
-        //println!("{}", count);
         if count ==0{
             break;
         }
@@ -14,20 +13,74 @@ fn handle_client(mut s : TcpStream) -> &'static str{
         s.write_all(response.as_bytes()).expect("stream should have written");
 
     }
-    
-    //s.write_all(response.as_bytes()).expect("stream should have written");
-    "asdsf"
 }
+
+
+struct Pool {
+    capacity : usize,
+    workers: Vec<Worker>,
+}
+
+impl Pool{
+    fn new() -> Self{
+        Self {
+            capacity : 10,
+            workers: {
+                let mut w = Vec::with_capacity(10);
+                for i in 0..10{
+                    w.push(Worker{is_available : true});
+                }
+                w
+            }
+        }
+    }
+
+    fn execute<F>(&mut self, handler_function : F)
+    where
+        F: FnOnce() -> () + Send + 'static  
+    {
+        for i in 0..self.capacity{
+            if self.workers[i].can_work(){
+                self.workers[i].do_work();
+                self.workers[i].work(handler_function);
+                break;
+            }
+        }
+
+        // std::thread::spawn(handler_function);
+    }
+}
+
+struct Worker{
+    is_available : bool,
+}
+
+impl Worker{
+    fn can_work(&self) -> bool{
+        self.is_available
+    }
+
+    fn do_work(&mut self){
+        self.is_available = false;
+    }
+
+    fn work<F> (&self, handler : F)
+    where
+        F: FnOnce() -> () + Send + 'static
+    {
+        std::thread::spawn(handler);
+    }
+}
+
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
+    let listener = TcpListener::bind("127.0.0.1:6370").unwrap();
+    let mut thread_pool = Pool::new(); //initiate_pool();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream);
-                println!("accepted  connection");
+                thread_pool.execute( || handle_client(stream));
+                //println!("accepted  connection");
             }
             Err(e) => {
                 println!("error: {}", e);
